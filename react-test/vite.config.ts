42e0c6a1f10225910487e78cd0988fc2a1602303
zhaoyukuan <zhaoyukuan95@gmail.com>
2025-08-21 20:47:25 +0800

    .

--- react-test/vite.config.ts
+++ react-test/vite.config.ts
@@ -4,7 +4,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: 'https://bucket-20250629.oss-cn-shanghai.aliyuncs.com',
+  base: 'https://bucket-20250629.oss-cn-shanghai.aliyuncs.com',
   plugins: [react(), viteCompression(),],
   build: {
     rollupOptions: {
